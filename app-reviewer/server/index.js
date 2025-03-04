f7a6d49bcf8727dbdee2f0942a266f7642cf85d3
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2025-03-04 19:11:51 +0530

    Added Llama local changes

--- app-reviewer/server/index.js
+++ app-reviewer/server/index.js
@@ -1,17 +1,28 @@
 import express from "express";
 import cors from "cors";
+import generateReview from "./review.js";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.post("/api/v1/reviews", (req, res) => {
+app.post("/api/v1/reviews", async (req, res) => {
   const code = req.body.code;
-  console.log(code);
-  res.send({
-    review: "Great job!",
-  });
+
+  try {
+    const review = await generateReview(code);
+    console.log(review,'review from server');
+
+    res.send({
+      review,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      review: "An error occurred while generating the review.",
+    });
+  }
 });
 
 app.listen(port, () => {
--- app-reviewer/src/components/Review.jsx
+++ app-reviewer/src/components/Review.jsx
@@ -4,8 +4,17 @@ import Loader from "./Loader";
 
 const Review = ({ review, isGenerating }) => {
   return (
-    <div className="h-full w-6/12 relative">
-      {isGenerating ? <Loader /> : <MarkDownPreview source={review} style={{ fontSize: "30px", height: "100vh" }} />}
+    <div className="h-full w-6/12 relative overflow-scroll">
+      {isGenerating ? (
+        <Loader />
+      ) : (
+        <MarkDownPreview
+          style={{ fontSize: "30px", minHeight: "100vh", padding: "20px" }}
+          source={JSON.stringify(review)}
+        >
+          {/* {typeof review === "string" ? review : ""} */}
+        </MarkDownPreview>
+      )}
     </div>
   );
 };
@@ -14,4 +23,5 @@ Review.propTypes = {
   review: PropTypes.string.isRequired,
   isGenerating: PropTypes.bool.isRequired,
 };
+
 export default Review;
